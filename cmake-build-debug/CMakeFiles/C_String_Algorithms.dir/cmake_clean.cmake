f54361cf69c84d3ffe7927636435657217bacc57
Cristian <cristian9918@hotmail.com>
2020-11-23 18:32:26 -0500

    first commit

---
+++ cmake-build-debug/CMakeFiles/C_String_Algorithms.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/C_String_Algorithms.dir/main.c.obj"
+  "C_String_Algorithms.exe"
+  "C_String_Algorithms.exe.manifest"
+  "C_String_Algorithms.pdb"
+  "libC_String_Algorithms.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/C_String_Algorithms.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/C_String_Algorithms.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for C_String_Algorithms.
+# This may be replaced when dependencies are built.
